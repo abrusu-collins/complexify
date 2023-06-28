de18a952b1d173bb100246a1a5ae6c56ce5c2f75
abrusu-collins <snillochemingson@gmail.com>
2023-06-28 01:11:50 +0000

    complexify is goated

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { FaCopy } from "react-icons/fa";
+import { Configuration, OpenAIApi } from "openai";
+import { Spinner } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [simpleSentence, setSimpleSentence] = useState("");
+  const [complexSentence, setComplexSentence] = useState("");
+  const toast = useToast();
+  const doNoting = (e) => {
+    e.preventDefault();
+  };
+  const complexify = (e) => {
+    e.preventDefault();
+    if (!simpleSentence) {
+      toast({
+        title: "No sentence added",
+        description: "Please fill the input field",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      return;
+    }
+    setIsLoading(true);
+    openai
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: `I'll give you a sentence, 
+        please rephrase it using complex english
+         that is very very barely understandable. Make it lengthy.
+          The sentence is ${simpleSentence}"
+      `,
+        temperature: 1,
+        max_tokens: 250,
+      })
+      .then((res) => {
+        return res;
+      })
+      .then((data) => {
+        setComplexSentence(data.data.choices[0].text);
+        console.log(data.data.choices[0].text);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const copy = (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(complexSentence);
+    toast({
+      title: "Copied",
+      description: "Text copied to clipboard",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
+  return (
+    <div className="home">
+      <p className="title"> Complexify</p>
+      <div className="grid">
+        <div className="examples">
+          <p>
+            Do you want to convert your simple sentence into one which is
+            complex and difficult to understand😂😂
+          </p>
+        </div>
+        <div className="form-and-results">
+          <textarea
+            placeholder="Enter your simple sentence here"
+            onChange={(e) => {
+              setSimpleSentence(e.target.value);
+            }}
+          />
+          <a href="" onClick={isLoading ? doNoting : complexify}>
+            {isLoading ? <Spinner size="md" /> : "Make it complex"}
+          </a>
+          {complexSentence && (
+            <div className="complex">
+              <p>{complexSentence}</p>
+              <a href="" onClick={copy}>
+                <FaCopy size={25} />
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
